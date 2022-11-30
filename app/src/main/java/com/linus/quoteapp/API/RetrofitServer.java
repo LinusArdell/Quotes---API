d9db94018dbfa11f2390fb83a480cdfbb362f68f
linus <linus2016chaesa@mhs.mdp.ac.id>
2022-11-30 14:54:48 +0700

    push

---
+++ app/src/main/java/com/linus/quoteapp/API/RetrofitServer.java
@@ -0,0 +1,14 @@
+package com.linus.quoteapp.API;
+
+import retrofit2.Retrofit;
+
+public class RetrofitServer {
+
+    private static final String baseUrl = "https://type.fit/api/";
+    private static Retrofit retro;
+
+    public static Retrofit connectRetrofit(){
+        
+    }
+
+}
