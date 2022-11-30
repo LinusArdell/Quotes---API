7c1f17dcfa394d5e9694ad23d75f32aeb39c41df
linus <linus2016chaesa@mhs.mdp.ac.id>
2022-11-30 14:49:51 +0700

    Commit

---
+++ app/src/main/java/com/linus/quoteapp/Model/QuotesModel.java
@@ -0,0 +1,13 @@
+package com.linus.quoteapp.Model;
+
+public class QuotesModel {
+    private String text, author;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
