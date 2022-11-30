103e33088a3100efa9aa64d38b8e085bec267f58
linus <linus2016chaesa@mhs.mdp.ac.id>
2022-11-30 14:25:25 +0700

    commit

---
+++ app/src/main/java/com/linus/quoteapp/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.linus.quoteapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.linus.quoteapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
